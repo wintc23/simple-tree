1788e252e5848ddbe91a2b080050e481b6d6ec55
Lushenggang <lushg-tcxg@qq.com>
2018-09-03 18:50:05 +0800

    simple-tree-init

---
+++ src/components/simple-tree/tree-node.vue
@@ -0,0 +1,271 @@
+<template>
+  <div
+    class="tree-node"
+    :class="nodeClass"
+    @dblclick="nodeDoubleClick"
+    @click="nodeClick">
+    <div
+      class="tree-node-content"
+      :draggable="canBeDrag"
+      @dragstart.stop="dragStart"
+      @dragend.stop="dragEnd"
+      @dragenter.stop="dragEnter"
+      @dragover.stop="dragOver"
+      @dragleave.stop="dragLeave"
+      @drop.stop="drop"
+      @click="contentClick"
+      @mouseenter="hover=true"
+      @mouseleave="hover=false"
+      ref="treeContent"
+      @dbclick="contentDoubleClick">
+      <div class="expand-button" @click.stop="expanded=!expanded" v-if="expand">
+        <template v-if="nodeData[props.children] && nodeData[props.children].length">
+          <Icon type="ios-arrow-dropdown" v-if="expanded"/>
+          <Icon type="ios-arrow-dropright" v-else/>
+        </template>
+        <template v-else>
+          <div class="expand-dot"></div>
+        </template>
+      </div>
+      <div class="node-render">
+        <div
+          v-if="dragHover && this.dragInfo.before"
+          class="node-drag-helper">
+          <div
+            :class="{
+              'drag-position': true,
+              'drop': this.dropPosition === 'before'
+            }"
+            @dragenter="dragPosition('before')"
+            @dragleave="dragPosition('')">
+            拖为前置节点
+          </div>
+        </div>
+        <node-content class="node-render-content"></node-content>
+        <div
+          v-if="dragHover && (this.dragInfo.after || this.dragInfo.inner)"
+          class="node-drag-helper">
+          <div
+            :class="{
+              'drag-position': true,
+              'drop': this.dropPosition === 'after'
+            }"
+            @dragenter="dragPosition('after')"
+            @dragleave="dragPosition('')"
+            v-if="this.dragInfo.after">
+            拖为后置节点
+          </div>
+          <div
+            :class="{
+              'drag-position': true,
+              'drop': this.dropPosition === 'inner'
+            }"
+            @dragenter="dragPosition('inner')"
+            @dragleave="dragPosition('')"
+            v-if="this.dragInfo.inner">
+            拖为子节点
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      :class="{
+        'tree-node-children': true,
+        'children-hide': dragging
+      }"
+      :style="childrenStyle"
+      v-if="nodeData[props.children] && nodeData[props.children].length"
+      v-show="expanded">
+      <tree-node
+        v-for="(child, idx) of nodeData[props.children]"
+        :key="idx"
+        :nodeData="child"
+        :nodeClass="nodeClass"
+        :allowDrag="allowDrag"
+        :dragInfo="dragInfo"
+        :draggable="draggable"
+        :expand="expand"
+        :indentLineColor="indentLineColor"
+        :indentWidth="indentWidth"
+        :props="props">
+      </tree-node>
+    </div>
+  </div>
+</template>
+
+<script type="text/jsx">
+export default {
+  name: 'tree-node',
+  props: {
+    nodeData: {
+      type: Object,
+      required: true
+    },
+    props: {
+      type: Object,
+      required: true
+    },
+    nodeClass: {
+      type: [Object, Array, String]
+    },
+    expand: {
+      type: Boolean,
+      default: true
+    },
+    allowDrag: Function,
+    dragInfo: Object,
+    draggable: Boolean,
+    indentLineColor: Array,
+    indentWidth: Number
+  },
+  components: {
+    'node-content': {
+      render (h) {
+        const parent = this.$parent
+        const tree = parent.tree
+        const data = parent.nodeData
+        const parentData = parent.parentData
+        return (
+          tree.$scopedSlots.default
+            ? tree.$scopedSlots.default({ data, parentData })
+            : <div>{data[parent.props.title]}</div>
+        )
+      }
+    }
+  },
+  computed: {
+    canBeDrag () {
+      return this.draggable && this.allowDrag && this.allowDrag(this.nodeData)
+    },
+    dragHover () {
+      return this === this.dragInfo.dropData
+    },
+    childrenStyle () {
+      let style = {
+        marginLeft: this.indentWidth + 'px'
+      }
+      let length = this.indentLineColor.length
+      if (length) {
+        let colorIndx = (this.level % length + length - 1) % length
+        let color = this.indentLineColor[colorIndx]
+        style.borderLeft = `1px ${this.hover ? 'solid' : 'dotted'} ${color}`
+      }
+      return style
+    }
+  },
+  data () {
+    return {
+      tree: null,
+      parentData: null,
+      level: 1,
+      expanded: true,
+      dragging: false,
+      dropPosition: '',
+      hover: false
+    }
+  },
+  watch: {
+    dragHover () {
+      if (!this.dragHover) {
+        this.dropPosition = ''
+      }
+    }
+  },
+  created () {
+    const parent = this.$parent
+    if (parent.isTree) {
+      this.tree = parent
+      this.parentData = parent.rootData
+      this.level = 1
+    } else {
+      this.tree = parent.tree
+      this.level = parent.level + 1
+      this.parentData = parent.nodeData
+    }
+  },
+  methods: {
+    nodeClick (event) {
+      this.tree.$emit('node-click', event, this)
+    },
+    nodeDoubleClick (event) {
+      this.tree.$emit('node-double-click', event, this)
+    },
+    contentClick (event) {
+      this.tree.$emit('content-click', event, this)
+    },
+    contentDoubleClick (event) {
+      this.tree.$emit('content-double-click', event, this)
+    },
+    /**
+     * 拖动相关接口，涉及多个节点，此处弹射事件均在tree.vue中处理
+     * 组件调用应该监听由tree.vue弹射的事件
+     */
+    dragStart (event) {
+      this.hideChildren()
+      this.tree.$emit('tree-node-drag-start', event, this)
+    },
+    dragEnd (event) {
+      this.recoverChildren()
+      this.tree.$emit('tree-node-drag-end', event, this)
+    },
+    dragEnter (event) {
+      this.tree.$emit('tree-node-drag-enter', event, this, this.$refs.treeContent)
+    },
+    dragOver (event) {
+      event.preventDefault()
+      this.tree.$emit('tree-node-drag-over', event, this, this.$refs.treeContent)
+    },
+    dragLeave (event) {
+      this.tree.$emit('tree-node-drag-leave', event, this, this.$refs.treeContent)
+    },
+    drop (event) {
+      this.tree.$emit('tree-node-drop', event, this)
+    },
+    hideChildren () {
+      this.dragging = true
+    },
+    recoverChildren () {
+      this.dragging = false
+    },
+    dragPosition (position) {
+      this.dropPosition = position
+      this.tree.$emit('tree-node-drag-position', position)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.tree-node
+  .tree-node-content
+    display flex
+    align-items center
+    .expand-button
+      width 1rem
+      height 1rem
+      font-size 1rem
+      font-weight bolder
+      display flex
+      align-items center
+      justify-content center
+      .expand-dot
+        border 3px solid gray
+        border-radius 3px
+    .node-render
+      flex auto
+      .node-drag-helper
+        display flex
+        .drag-position
+          margin 2px
+          margin-right 1rem
+          padding 2px 0.5rem
+          box-shadow 0 0 1px 1px #FFD04C
+          border-radius 5px
+          &.drop
+            background #FFC82C
+            color red
+            font-weight bolder
+  .tree-node-children
+    &.children-hide
+      display none
+</style>
---
+++ src/components/simple-tree/tree.vue
@@ -0,0 +1,242 @@
+<template>
+  <div class="tree">
+    <tree-node
+      v-for="(nodeData, idx) of treeData"
+      :nodeData="nodeData"
+      :props="props"
+      :nodeClass="nodeClass"
+      :expand="expand"
+      :allowDrag="allowDrag"
+      :dragInfo="dragInfo"
+      :draggable="draggable"
+      :indentLineColor="indentLineColor"
+      :indentWidth="indentWidth"
+      :key="idx">
+    </tree-node>
+  </div>
+</template>
+
+<script>
+import treeNode from './tree-node.vue'
+
+export default {
+  props: {
+    treeData: Array,
+    props: {
+      type: Object,
+      default: () => {
+        return {
+          children: 'children',
+          title: 'title',
+          id: 'id'
+        }
+      }
+    },
+    nodeClass: [Object, Array, String],
+    expand: {
+      type: Boolean,
+      default: true
+    },
+    allowDrop: {
+      type: Function,
+      default: () => true
+    },
+    allowDrag: {
+      type: Function,
+      default: () => true
+    },
+    draggable: {
+      type: Boolean,
+      default: false
+    },
+    indentLine: [Array, Boolean],
+    indentLimit: Number,
+    maxIndent: {
+      type: Number,
+      default: 20
+    }
+  },
+  components: {
+    'tree-node': treeNode
+  },
+  data () {
+    return {
+      isTree: true,
+      treeWidth: 0,
+      observer: null,
+      dragInfo: {
+        dragData: null,
+        dropData: null,
+        position: '',
+        before: true,
+        after: true,
+        inner: true
+      }
+    }
+  },
+  computed: {
+    rootData () {
+      let data = { isRoot: true }
+      data[this.props.children] = this.treeData
+      return data
+    },
+    indentLineColor () {
+      let colorList = []
+      if (typeof this.indentLine === 'boolean' && this.indentLine) {
+        colorList = ['red', 'green', 'blue']
+      } else if (Array.isArray(this.indentLine)) {
+        colorList = this.indentLine
+      }
+      return colorList
+    },
+    indentWidth () {
+      if (!this.indentLimit || this.indentLimit <= 0 || !this.treeWidth) {
+        return this.maxIndent
+      }
+      let limit = this.indentLimit < 100 ? this.treeWidth * this.indentLimit / 100 : this.indentLimit
+      let indent = Math.round(Math.min(limit / this.level, this.maxIndent))
+      return indent
+    },
+    level () {
+      return this.getMaxLevel()
+    }
+  },
+  created () {
+    this.registerDragHandle()
+  },
+  mounted () {
+    this.treeWidth = Math.round(this.$el.getBoundingClientRect().width)
+    this.registerObserver()
+  },
+  beforeDestroy () {
+    this.removeObserver()
+    this.$off('tree-node-drag-start')
+    this.$off('tree-node-drag-end')
+    this.$off('tree-node-drag-enter')
+    this.$off('tree-node-drag-position')
+    this.$off('tree-node-drag-over')
+    this.$off('tree-node-drag-leave')
+    this.$off('tree-node-drop')
+  },
+  methods: {
+    checkAllowDrop () {
+      if (!this.allowDrop || !this.dragInfo.dragData || !this.dragInfo.dropData || !this.dragInfo.position) {
+        return false
+      }
+      if (typeof this.allowDrop === 'boolean') {
+        return this.allowDrop
+      }
+      return this.allowDrop(this.dragInfo.dragData, this.dragInfo.dropData, this.dragInfo.position) || false
+    },
+    registerDragHandle () {
+      this.$on('tree-node-drag-start', (event, vNode) => {
+        this.dragInfo.dragData = vNode
+        this.$emit('tree-drag-start', event, vNode)
+      })
+      this.$on('tree-node-drag-end', (event, vNode) => {
+        this.clearDragInfo()
+        this.$emit('tree-drag-end', event, vNode)
+      })
+      this.$on('tree-node-drag-enter', (event, vNode, target) => {
+        this.setDropNode(vNode)
+        this.$emit('tree-drag-enter', event, vNode)
+      })
+      this.$on('tree-node-drag-over', (event, vNode, target) => {
+        this.$emit('tree-drag-over', event, vNode)
+      })
+      this.$on('tree-node-drag-position', (position) => {
+        this.dragInfo.position = position
+        this.$emit('tree-drag-position', event, position)
+      })
+      this.$on('tree-node-drag-leave', (event, vNode, target) => {
+        if (this.eventInElement(event, target)) return
+        this.dragInfo.dropData = null
+        this.$emit('tree-drag-leave', event, vNode)
+      })
+      this.$on('tree-node-drop', (event, vNode) => {
+        if (this.checkAllowDrop()) {
+          this.$emit('tree-drop', this.dragInfo.dragData, this.dragInfo.dropData, this.dragInfo.position)
+        }
+        this.clearDragInfo()
+      })
+    },
+    clearDragInfo () {
+      this.dragInfo = {
+        dragData: null,
+        dropData: null,
+        position: '',
+        before: true,
+        after: true,
+        inner: true
+      }
+    },
+    eventInElement (event, node) {
+      let rect = node.getBoundingClientRect()
+      return rect.left < event.clientX && event.clientX < rect.right && rect.top < event.clientY && event.clientY < rect.bottom
+    },
+    setDropNode (vNode) {
+      let drag = this.dragInfo.dragData
+      if (!drag || drag === vNode) return
+      let drop = this.dragInfo.dropData
+      if (drop && drop === vNode) return
+      this.dragInfo.dropData = vNode
+      drop = this.dragInfo.dropData
+      let childrenKey = this.props.children
+      let before = true
+      let after = true
+      let inner = true
+      if (drop.nodeData[childrenKey] && drop.nodeData[childrenKey].includes(drag.nodeData)) {
+        inner = false
+      }
+      if (drop.parentData === drag.parentData) {
+        let dragIndex = drag.parentData[childrenKey].indexOf(drag.nodeData)
+        let dropIndex = drop.parentData[childrenKey].indexOf(drop.nodeData)
+        if (dragIndex === dropIndex - 1) {
+          before = false
+        } else if (dragIndex - 1 === dropIndex) {
+          after = false
+        }
+      }
+      if (typeof this.allowDrop === 'function') {
+        this.dragInfo.before = before && this.allowDrop(drag, drop, 'before')
+        this.dragInfo.after = after && this.allowDrop(drag, drop, 'after')
+        this.dragInfo.inner = inner && this.allowDrop(drag, drop, 'inner')
+      }
+    },
+    getMaxLevel () {
+      let level = 0
+      for (let node of this.treeData) {
+        let nodeLevel = this.getLevel(node)
+        level = Math.max(level, nodeLevel)
+      }
+      return level
+    },
+    getLevel (data) {
+      let children = data[this.props.children] || []
+      if (!children.length) {
+        return 0
+      }
+      let level = 0
+      for (let node of children) {
+        let nodeLevel = this.getLevel(node)
+        level = Math.max(nodeLevel, level)
+      }
+      return level + 1
+    },
+    registerObserver () {
+      let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+      this.observer = new MutationObserver(() => {
+        this.treeWidth = Math.round(this.$el.getBoundingClientRect().width)
+      })
+      this.observer.observe(document.body, { attributes: true, subtree: true })
+    },
+    removeObserver () {
+      if (this.observer) {
+        this.observer.disconnect()
+        this.observer.takeRecords()
+        this.observer = null
+      }
+    }
+  }
+}
+</script>
